d77fb6b281107b0be0caedea642f0b0f1abda6f7
Ian P. Cordero <ianpcordero@gmail.com>
2015-11-04 01:26:53 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//
+// main.js
+// Prime3BFFs
+//
+// Created by Ian Cordero, Elise Yuen, Colin Fong, and Lena Correia on 11/3/15.
+// Copyright (c) 2015 UCLA Daily Bruin Online. All rights reserved.
+//
+
+$(document).ready(function() {
+    // Initialize fullpage
+    $('#fullpage').fullpage();
+});
